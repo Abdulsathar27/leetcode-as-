f040d2ac893ee9b7fcfed7b69cae2b05e0541603
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2025-10-06 10:50:24 +0530

    next level leetcode

---
+++ 2235..dart
@@ -0,0 +1,16 @@
+class Solution {
+  int sum(int num1, int num2) {
+    return num1 + num2;
+  }
+}
+
+void main() {
+  var sol = Solution();
+  print(sol.sum(12, 5));  
+  print(sol.sum(-10, 4)); 
+}
+
+// This Dart code defines a Solution class with a simple method called sum that performs basic integer addition.
+// The method takes two integer arguments, num1 and num2, and returns their sum.
+// The main function demonstrates this by creating an instance of Solution and printing
+// the results of adding the two provided pairs of numbers.
